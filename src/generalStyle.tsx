6f7a48a8a2ef46a30341d34ea82946aafab780cc
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-09-09 15:29:28 +0300

    Section About Teacher of Stories

---
+++ src/generalStyle.tsx
@@ -0,0 +1,25 @@
+export const fontSizes={
+    paragraph:{
+        small:"16px",
+        base:"20px",
+        large:"24px"
+    },
+    heading:{
+        small:"60px",
+        base:"80px",
+        large:"100px"
+    }
+}
+
+export const colors={
+    primary:{
+        base:"#002366",
+        lighter:"#234B92FF",
+        darker:"#005"
+    },
+    secondary:{
+       base:"#b4a087",
+       darker:"#8d7056",
+        lighter:"#e7c9ae"
+    }
+}
