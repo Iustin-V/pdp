d0d251e270fbb90cf34ad3292715c74dbc969acf
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-09-14 13:32:02 +0300

    fix

--- src/comp/Partners/Partners.tsx
+++ src/comp/Partners/Partners.tsx
@@ -8,30 +8,38 @@ import PointsOfYouLogo from '../images/Partners/pointsofyou-logo.png'
 
 import styled from "styled-components";
 
-export const TitleReviewSection = styled.img`
+export const LogoSection = styled.img`
 width: 276px;
 `
 export const FlexWrapper = styled.div`
   display: flex;
   justify-content: space-around;
-  flex-wrap: wrap;
-  
+  flex-wrap: wrap;'
+`
+
+export const Container = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  padding: 40px;
+  margin: auto;
+  @media (max-width: 1200px) {
+    max-width: 800px;
+    flex-direction: column;
+  }
 `
 
 
 export const Partners = () => {
     return (
-        <PageContainer>
-            <SectionContainer>
+            <Container>
                 <TitleSection color={colors.primary.base}>
                     Parteneri
                 </TitleSection>
                 <FlexWrapper>
-                <TitleReviewSection src={OanaNiculescuLogo}/>
-                <TitleReviewSection src={LectiiViraleLogo}/>
-                <TitleReviewSection src={PointsOfYouLogo}/>
+                <LogoSection src={OanaNiculescuLogo}/>
+                <LogoSection src={LectiiViraleLogo}/>
+                <LogoSection src={PointsOfYouLogo}/>
                 </FlexWrapper>
-                </SectionContainer>
-        </PageContainer>
+                </Container>
     );
 };
