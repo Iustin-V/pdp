3ede9222da11a093d375eb8332e016b45b7e01bb
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-09-15 09:55:44 +0300

    fix5

--- src/comp/Partners/Partners.tsx
+++ src/comp/Partners/Partners.tsx
@@ -1,26 +1,28 @@
-import React from "react";
-import {PageContainer, SectionContainer, TitleSection} from "../Styles";
+import React, {useEffect, useState} from "react";
+import {TitleSection} from "../Styles";
 import {colors} from "../../generalStyle";
 import OanaNiculescuLogo from '../images/Partners/oananiculescu-logo.png'
 import LectiiViraleLogo from '../images/Partners/lectiivirale-logo.png'
 import PointsOfYouLogo from '../images/Partners/pointsofyou-logo.png'
+import http from '../../http';
 
 
 import styled from "styled-components";
 
 export const LogoSection = styled.img`
-width: 276px;
+  width: 276px;
 `
 export const FlexWrapper = styled.div`
   display: flex;
   justify-content: space-around;
-  flex-wrap: wrap;'
+  flex-wrap: wrap;
+'
 `
 
 export const Container = styled.div`
   max-width: 1440px;
   width: calc(100% - 80px);
-  padding:40px;
+  padding: 40px;
   margin: auto;
   @media (max-width: 1200px) {
     max-width: 800px;
@@ -30,16 +32,44 @@ export const Container = styled.div`
 
 
 export const Partners = () => {
+    //
+    // const [pets, setPets] = useState("");
+    // const [nav_value, set_nav_value] = useState("PetList");
+    // const [petId, setPetId] = useState("");
+    // const getPetId = (id:string) => {
+    //     setPetId(id);
+    // };
+    //
+    // // @ts-ignore
+    // useEffect(() => {
+    //     const readAllPets = async () => {
+    //         const response = await http.get("/api/pets");
+    //         const responseArr = Object.values(response.data.data);
+    //         console.log(response)
+    //         // @ts-ignore
+    //         setPets(responseArr);
+    //     };
+    //     return readAllPets;
+    // }, []);
+    function redirectPartnerOana() {
+        window.location.href = "https://oananiculae.com/";
+    }
+    function redirectPointsOfYou() {
+        window.location.href = "https://www.points-of-you.ro/";
+    }
+    function redirectLectiiVirale() {
+        window.location.href = "https://lectii-virtuale.ro/";
+    }
     return (
-            <Container>
-                <TitleSection color={colors.primary.base}>
-                    Parteneri
-                </TitleSection>
-                <FlexWrapper>
-                <LogoSection src={OanaNiculescuLogo}/>
-                <LogoSection src={LectiiViraleLogo}/>
-                <LogoSection src={PointsOfYouLogo}/>
-                </FlexWrapper>
-                </Container>
+        <Container>
+            <TitleSection color={colors.primary.base}>
+                Parteneri
+            </TitleSection>
+            <FlexWrapper>
+                <LogoSection onClick={redirectPartnerOana} src={OanaNiculescuLogo}/>
+                <LogoSection onClick={redirectPointsOfYou} src={LectiiViraleLogo}/>
+                <LogoSection onClick={redirectLectiiVirale}  src={PointsOfYouLogo}/>
+            </FlexWrapper>
+        </Container>
     );
 };
