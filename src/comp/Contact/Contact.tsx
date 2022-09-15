6380effb3b6ff78c538dd010bac0d92f96ee7caa
Iustin Vasiliu <iustinv2.0@gmail.com>
2022-09-15 19:29:02 +0300

    contact form update

--- src/comp/Contact/Contact.tsx
+++ src/comp/Contact/Contact.tsx
@@ -1,12 +1,47 @@
-import { SectionContainer, StyledPageWrapper } from "../Styles";
 import styled from "styled-components";
+
+import { StyledPageWrapper } from "../Styles";
+
 const StyledForm = styled.form`
-  background: #f8f4e5;
+  border-radius: 5px;
+  background-color: #f2f2f2;
   padding: 50px 100px;
-  border: 2px solid rgba(0, 0, 0, 1);
-  box-shadow: 15px 15px 1px #b4a087, 15px 15px 1px 2px rgba(0, 0, 0, 1);
 `;
 const StyledInput = styled.input`
+  display: block;
+  width: 100%;
+  font-size: 24px;
+  line-height: 24px * 2;
+  font-family: "Fjalla One";
+  margin-bottom: 24px * 2;
+  border: none;
+  border-bottom: 5px solid rgba(0, 0, 0, 1);
+  background: #f8f4e5;
+  min-width: 250px;
+  padding-left: 5px;
+  outline: none;
+  color: rgba(0, 0, 0, 1);
+`;
+const StyledButton = styled.input`
+  height: 40px;
+  display: block;
+  margin: 0 auto;
+  line-height: 24px * 2;
+  padding: 0 20px;
+  background: #b4a087;
+  letter-spacing: 2px;
+  transition: 0.2s all ease-in-out;
+  outline: none;
+  border: 1px solid rgba(0, 0, 0, 1);
+
+  :hover {
+    background: #f8f4e5;
+  }
+`;
+const StyledContainer = styled.div`
+  margin: 20px 0;
+`;
+const StyledTextArea = styled.textarea`
   display: block;
   width: 100%;
   font-size: 24px;
@@ -21,27 +56,20 @@ const StyledInput = styled.input`
   outline: none;
   color: rgba(0, 0, 0, 1);
 `;
-const StyledButton = styled.input`
-  height: 30px;
+const StyledSelect = styled.select`
   display: block;
-  margin: 0 auto;
+  width: 100%;
+  font-size: 24px;
   line-height: 24px * 2;
-  padding: 0 20px;
-  background: #b4a087;
-  letter-spacing: 2px;
-  transition: 0.2s all ease-in-out;
+  font-family: "Fjalla One";
+  margin-bottom: $font-size * 2;
+  border: none;
+  border-bottom: 5px solid rgba(0, 0, 0, 1);
+  background: #f8f4e5;
+  min-width: 250px;
+  padding-left: 5px;
   outline: none;
-  border: 1px solid rgba(0, 0, 0, 1);
-  box-shadow: 3px 3px 1px 1px #95a4ff, 3px 3px 1px 2px rgba(0, 0, 0, 1);
-
-  :hover {
-    background: rgba(0, 0, 0, 1);
-    color: white;
-    border: 1px solid rgba(0, 0, 0, 1);
-  }
-`;
-const StyledContainer = styled.div`
-  margin: 20px 0;
+  color: rgba(0, 0, 0, 1);
 `;
 export const Contact = () => {
   return (
@@ -81,12 +109,22 @@ export const Contact = () => {
           />
         </StyledContainer>
         <StyledContainer>
-          {/*<label htmlFor="email">Mesaj</label>*/}
-          <StyledInput
+          <StyledSelect
+            id="course"
+            name="Course"
+            placeholder="Selecteaza cursul dorit"
+          >
+            <option value="course1">Curs dezvoltare personala</option>
+            <option value="course2">Curs 2</option>
+            <option value="course3">Curs 3</option>
+          </StyledSelect>
+        </StyledContainer>
+        <StyledContainer>
+
+        <StyledTextArea
             placeholder="Message"
             name="Message"
             id="nessage"
-            type="text"
             required
           />
         </StyledContainer>
