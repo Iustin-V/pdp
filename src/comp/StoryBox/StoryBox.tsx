51d61265e530def7e228952fb634677a9a11f2ce
paraschivVali <valyparaschiv05@gmail.com>
2022-10-03 20:16:36 +0300

    chore: Story box page created

---
+++ src/comp/StoryBox/StoryBox.tsx
@@ -0,0 +1,11 @@
+import { StoryBoxPageContainer } from "./StoryBoxStyle";
+import {WhatHowAndWhySection} from "./whatHowAndWhy"
+import {StoryBoxSection} from "./StoryBoxSection";
+export const StoryBoxPage = () => {
+  return (
+    <StoryBoxPageContainer>
+      <WhatHowAndWhySection />
+        <StoryBoxSection />
+    </StoryBoxPageContainer>
+  );
+};
