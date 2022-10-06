d2387e9347fb797e0925b8be74613eaa659aa19c
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-10-06 10:40:20 +0300

    fix_New_proxy_5

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,23 +6,23 @@ const useFetch = (url) => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true)
-            
-            try {
-                const response = await fetch("https://api-example2.onrender.com/api/sections")
-                const res = response.json()
-
-                setData(res.data)
-                console.log(data, 'data from useFetch')
-            } catch (err) {
-                setError(err)
-            }
-            ;setLoading(false)
-        };
-        fetchData();
-    }, [url])
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         setLoading(true)
+    //
+    //         try {
+    //             const response = await fetch("https://api-example2.onrender.com/api/sections")
+    //             const res = response.json()
+    //
+    //             setData(res.data)
+    //             console.log(data, 'data from useFetch')
+    //         } catch (err) {
+    //             setError(err)
+    //         }
+    //         ;setLoading(false)
+    //     };
+    //     fetchData();
+    // }, [url])
 
     const reFetch = async () => {
         setLoading(true)
