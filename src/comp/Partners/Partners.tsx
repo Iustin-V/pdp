6dcb3ac41fbcd357b2e460145b40ef3d568772b8
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-10-06 10:03:08 +0300

    fix_New_proxy_4

--- src/comp/Partners/Partners.tsx
+++ src/comp/Partners/Partners.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {TitleSection} from "../Styles";
 import {colors} from "../../generalStyle";
 import OanaNiculescuLogo from "../images/Partners/oananiculescu-logo.png";
@@ -6,7 +6,7 @@ import LectiiViraleLogo from "../images/Partners/lectiivirale-logo.png";
 import PointsOfYouLogo from "../images/Partners/pointsofyou-logo.png";
 
 import styled from "styled-components";
-import useFetch from "../../hooks/useFetch";
+import axios from "axios";
 
 export const LogoSection = styled.img`
   width: 276px;
@@ -30,10 +30,27 @@ export const Container = styled.div`
 `;
 
 export const Partners = () => {
-        console.log('start')
-        const {data, loading, error} = useFetch("/sections")
-        console.log(data, 'data from fetch partners')
+        // console.log('start')
+        // const {data, loading, error} = useFetch("/sections")
+        // console.log(data, 'data from fetch')
 
+        const [error, setError] = useState(null);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [items, setItems] = useState([]);
+
+        const getData = async () => {
+            try {
+                const result = await axios("https://api-example2.onrender.com/api/sections")
+                console.log(result)
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        useEffect(() => {
+            getData()
+        });
 
         function redirectPartnerOana() {
             window.location.href = "https://oananiculae.com/";
