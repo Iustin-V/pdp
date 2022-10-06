019c3f677c5a63f278181b904fdbdf99d0fac8de
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-10-06 09:12:42 +0300

    fix_New_proxy_3

--- src/comp/Partners/Partners.tsx
+++ src/comp/Partners/Partners.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {TitleSection} from "../Styles";
 import {colors} from "../../generalStyle";
 import OanaNiculescuLogo from "../images/Partners/oananiculescu-logo.png";
@@ -6,7 +6,6 @@ import LectiiViraleLogo from "../images/Partners/lectiivirale-logo.png";
 import PointsOfYouLogo from "../images/Partners/pointsofyou-logo.png";
 
 import styled from "styled-components";
-import useFetch from "../../hooks/useFetch";
 
 export const LogoSection = styled.img`
   width: 276px;
@@ -30,30 +29,55 @@ export const Container = styled.div`
 `;
 
 export const Partners = () => {
-    console.log('start')
-    const {data, loading, error} = useFetch("/sections")
-    console.log(data, 'data from fetch')
+        // console.log('start')
+        // const {data, loading, error} = useFetch("/sections")
+        // console.log(data, 'data from fetch')
 
-    function redirectPartnerOana() {
-        window.location.href = "https://oananiculae.com/";
+        const [error, setError] = useState(null);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [items, setItems] = useState([]);
+
+
+        useEffect(() => {
+            fetch("https://api-example2.onrender.com/api/sections")
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setItems(result);
+                        console.log(result,'result final')
+                    },
+                    // Note: it's important to handle errors here
+                    // instead of a catch() block so that we don't swallow
+                    // exceptions from actual bugs in components.
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                )
+        }, [])
+
+        function redirectPartnerOana() {
+            window.location.href = "https://oananiculae.com/";
+        }
+
+        function redirectPointsOfYou() {
+            window.location.href = "https://www.points-of-you.ro/";
+        }
+
+        function redirectLectiiVirale() {
+            window.location.href = "https://lectii-virtuale.ro/";
+        }
+
+        return (
+            <Container>
+                <TitleSection color={colors.primary.base}>Parteneri</TitleSection>
+                <FlexWrapper>
+                    <LogoSection onClick={redirectPartnerOana} src={OanaNiculescuLogo}/>
+                    <LogoSection onClick={redirectLectiiVirale} src={LectiiViraleLogo}/>
+                    <LogoSection onClick={redirectPointsOfYou} src={PointsOfYouLogo}/>
+                </FlexWrapper>
+            </Container>
+        );
     }
-
-    function redirectPointsOfYou() {
-        window.location.href = "https://www.points-of-you.ro/";
-    }
-
-    function redirectLectiiVirale() {
-        window.location.href = "https://lectii-virtuale.ro/";
-    }
-
-    return (
-        <Container>
-            <TitleSection color={colors.primary.base}>Parteneri</TitleSection>
-            <FlexWrapper>
-                <LogoSection onClick={redirectPartnerOana} src={OanaNiculescuLogo}/>
-                <LogoSection onClick={redirectLectiiVirale} src={LectiiViraleLogo}/>
-                <LogoSection onClick={redirectPointsOfYou} src={PointsOfYouLogo}/>
-            </FlexWrapper>
-        </Container>
-    );
-};
+;
