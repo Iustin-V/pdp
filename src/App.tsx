d9f0594662a1088422c028a111d8c800b386cecf
Iustin Vasiliu <iustinv2.0@gmail.com>
2022-10-17 09:31:00 +0300

    routes fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./App.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Home} from "./comp/Home";
-import {StyledWrapper} from "./comp/Styles";
-import {Courses} from "./comp/Courses";
-import {WeekStorySection} from "./comp/WeekStorySection/WeekStorySection";
-import {Navbar} from "./comp/Navbar";
-import {ToTopButton} from "./comp/ToTopButton";
-import {Footer} from "./comp/Footer";
-import {WorkInProgress} from "./comp/WorkInProgress";
-import {Contact} from "./comp/Contact/Contact";
-import {Events} from "./comp/Events/Events";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./comp/Home";
+import { StyledWrapper } from "./comp/Styles";
+import { Courses } from "./comp/Courses";
+import { WeekStorySection } from "./comp/WeekStorySection/WeekStorySection";
+import { Navbar } from "./comp/Navbar";
+import { ToTopButton } from "./comp/ToTopButton";
+import { Footer } from "./comp/Footer";
+import { WorkInProgress } from "./comp/WorkInProgress";
+import { Contact } from "./comp/Contact/Contact";
+import { Events } from "./comp/Events/Events";
 import ScrollToTop from "./ScrollToTop";
 import linkGenerate from "./generalFunction";
-import {CourseInfo} from "./comp/CourseInfo/CouseInfo";
+import { CourseInfo } from "./comp/CourseInfo/CouseInfo";
 import Login from "./comp/Login/Login";
 import useFetch from "./hooks/useFetch";
 import { getData } from "./utils/getData";
-import {StoryBoxPage} from "./comp/StoryBox/StoryBox";
-
+import { StoryBoxPage } from "./comp/StoryBox/StoryBox";
 
 export const PDPContext = React.createContext({});
 
 export interface Category {
-    content: string[];
-    img: string[];
-    language: string;
-    sectionName: string;
-    subTitle: string[];
-    video: string[];
-    links: string[];
-    contain: string[];
-    title: string;
-    _id: string;
-    __v: number;
+  content: string[];
+  img: string[];
+  language: string;
+  sectionName: string;
+  subTitle: string[];
+  video: string[];
+  links: string[];
+  contain: string[];
+  title: string;
+  _id: string;
+  __v: number;
 }
 
 function App() {
-    const topRef = React.useRef<null | HTMLDivElement>(null);
-    const [isTopButton, setTopButton] = React.useState(false);
-    const [allCategories, setAllCategories] = React.useState([]);
-    const [navbarText, setNavbarTexts] = useState([]);
-    const [allLinkCourses, setLinkCourses] = useState([{title: '', price: '', time: '', text: [], image: ""}])
-    const [linkPathCourses, setLinkPathCourses] = useState('')
+  const topRef = React.useRef<null | HTMLDivElement>(null);
+  const [isTopButton, setTopButton] = React.useState(false);
+  const [allCategories, setAllCategories] = React.useState([]);
+  const [navbarText, setNavbarTexts] = useState([]);
+  const [allLinkCourses, setLinkCourses] = useState([
+    { title: "", price: "", time: "", text: [], image: "" },
+  ]);
+  const [linkPathCourses, setLinkPathCourses] = useState("");
 
   const { data, loading, error } = useFetch(
     `https://api-example2.onrender.com/api/sections/sectionByLanguage?language=${
@@ -57,54 +58,70 @@ function App() {
       // @ts-ignore
       setAllCategories(data);
       setNavbarTexts(getData(data, "Navbar").subTitle);
+      const allData = getData(data, "CourseChildParents").content.concat(
+        getData(data, "CourseTeacher").content
+      );
+      setLinkCourses(allData);
+    }
+    switch (localStorage.locale) {
+      case "ro":
+        setLinkPathCourses("cursuri");
+        break;
+      case "en":
+        setLinkPathCourses("courses");
+        break;
+      case "fr":
+        setLinkPathCourses("cours");
+        break;
     }
   }, [loading]);
 
-    const coursesLinkRoutes = allLinkCourses.map((link) => {
-        return (
-            <Route
-                path={`/${linkPathCourses}/${linkGenerate(link.title)}`}
-                element={<CourseInfo title={link.title} text={link.text}/>}
-            />
-        );
-    });
-    const arrayNavbarLinks = [
-        <StoryBoxPage/>,
-        <WeekStorySection/>,
-        <Courses/>,
-        <Events/>,
-        <Contact/>,
-    ];
-    const navbarLinks = navbarText.map((link, index) => {
-        return (
-            <Route
-                path={`/${linkGenerate(link)}`}
-                element={arrayNavbarLinks[index]}
-            />
-        );
-    });
-
+  const coursesLinkRoutes = allLinkCourses.map((link) => {
     return (
-        <StyledWrapper>
-            <PDPContext.Provider value={allCategories}>
-                <BrowserRouter>
-                    <div ref={topRef}/>
-                    <Navbar allCategories={allCategories} setTopButton={setTopButton}/>
-                    <ScrollToTop/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        {navbarLinks}
-                        <Route path="/blog" element={<WorkInProgress/>}/>
-                        <Route path="/login" element={<Login/>}/>
-
-                        {coursesLinkRoutes}
-                    </Routes>
-                    <ToTopButton topRef={topRef} isTopButton={isTopButton}/>
-                    <Footer/>
-                </BrowserRouter>
-            </PDPContext.Provider>
-        </StyledWrapper>
+      <Route
+        path={`/${linkPathCourses}/${linkGenerate(link.title)}`}
+        element={<CourseInfo title={link.title} text={link.text} />}
+      />
     );
+  });
+  const arrayNavbarLinks = [
+    <StoryBoxPage />,
+    <WeekStorySection />,
+    <Courses />,
+    <Events />,
+    <Contact />,
+  ];
+  const navbarLinks = navbarText.map((link, index) => {
+    console.log("link", linkGenerate(link), index);
+    return (
+      <Route
+        path={`/${linkGenerate(link)}`}
+        element={arrayNavbarLinks[index]}
+      />
+    );
+  });
+
+  return (
+    <StyledWrapper>
+      <PDPContext.Provider value={allCategories}>
+        <BrowserRouter>
+          <div ref={topRef} />
+          <Navbar allCategories={allCategories} setTopButton={setTopButton} />
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {navbarLinks}
+            <Route path="/blog" element={<WorkInProgress />} />
+            <Route path="/login" element={<Login />} />
+
+            {coursesLinkRoutes}
+          </Routes>
+          <ToTopButton topRef={topRef} isTopButton={isTopButton} />
+          <Footer />
+        </BrowserRouter>
+      </PDPContext.Provider>
+    </StyledWrapper>
+  );
 }
 
 export default App;
