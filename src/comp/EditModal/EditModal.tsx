9a9276c02e9e074d0b8209fb0662c014d54c9d8f
Iustin Vasiliu <iustinv2.0@gmail.com>
2022-10-22 13:30:33 +0300

    edit modal update final

--- src/comp/EditModal/EditModal.tsx
+++ src/comp/EditModal/EditModal.tsx
@@ -11,7 +11,6 @@ import {
 } from "./EditModalStyle";
 import { colors } from "../../generalStyle";
 import { capitalizeFirstLetter } from "../../utils/Capitalize";
-import Login from "../Login/Login";
 
 interface EditModalProps {
   modalData: any;
@@ -24,7 +23,6 @@ interface EditModalProps {
 export const EditModal = (props: EditModalProps) => {
   const localModalData = props?.modalData;
   const [updateObject, setUpdateObject] = React.useState({});
-  const [intermediaryObject, setUpdateIntermediaryObject] = React.useState({});
   const [updateArray, setUpdateArray] = React.useState([]);
   const [updateObjectArray, setUpdateObjectArray] = React.useState([{}]);
   const [initialArray, setInitialArray] = React.useState([]);
@@ -46,15 +44,17 @@ export const EditModal = (props: EditModalProps) => {
   }, [initialArray, initialObjectArray]);
 
   const saveModal = () => {
+    let content = undefined;
+    if (updateArray.length > 0) {
+      content = updateArray;
+    } else if (updateObjectArray.length > 0) {
+      content = updateObjectArray;
+    }
     axios.put(
       `https://pdp-api.onrender.com /api/sections/${localModalData?._id}`,
       {
         ...updateObject,
-        // content: ['Researchers have discovered that when we listen to stories, more chemicals are released in the brain associated with various emotional states. Thus, stories give meaning and emotion to our existence, motivate us and make us reflect on everything that happens around us. That\'s why stories chaaange behavior without resorting to the system of punishments and rewards.asddas','Les chercheurs ont découvert que lorsque nous écoutons des histoires, plus de produits chimiques sont libérés dans le cerveau associés à divers états émotionnels. Ainsi, les histoires donnent du sens et de l\'émotion à notre existence, nous motivent et nous font réfléchir sur tout ce qui se passe autour de nous. C\'est pourquoi les histoires changent les comportements sans recourir au système des punitions et des récompenses.','Nous avons assez de réunions, de conférences, de devoirs et de devoirs ! Les histoires sont amusantes, éducatives, motivantes et stimulent la créativité, ce qui nous aide dans tous les domaines de la vie. Les histoires nous donnent différents points de vue sur la même réalité et nous font trouver des solutions ingénieuses à des problèmes que je pensais peut-être insolubles.','Et des réponses aux questions non encore posées.'],
-        //@ts-ignore
-
-        // content: updateArray,
-        content: updateObjectArray,
+        content,
       }
     );
     exitModal();
@@ -64,24 +64,19 @@ export const EditModal = (props: EditModalProps) => {
     props.setEditModal(false);
     document.body.style.overflow = "unset";
   };
-  // console.log("localModalData", capitalizeFirstLetter);
 
   const handleMessageChange = (event: any) => {
-    //@ts-ignore
-    console.log("event.target.value", event.target.value);
+
 
     setUpdateObject({
       ...updateObject,
       [event.target.name]: event.target.value,
     });
 
-    console.log("updateObject", updateObject);
   };
 
   const handleArrayMessageChange = (event: any, index?: number) => {
-    console.log("initialArray", initialArray);
 
-    console.log("initialArray1", initialArray.slice(0, index));
     setInitialArray(props.modalData[event.target.name]);
     // @ts-ignore
     // la copy paste trb adaugat un space la final ca sa  citeasca
@@ -92,9 +87,7 @@ export const EditModal = (props: EditModalProps) => {
       ...updateArray.slice(index + 1, initialArray.length),
     ]);
 
-    console.log("verificare", updateArray.slice(0, index));
 
-    console.log("updateArray", updateArray);
   };
 
   const handleObjectArrayMessageChange = (
@@ -103,24 +96,17 @@ export const EditModal = (props: EditModalProps) => {
     index: number
   ) => {
     setInitialObjectArray(props.modalData["content"]);
-    setUpdateIntermediaryObject(updateObjectArray[index]);
-    console.log("updateObjectArray", updateObjectArray, event);
 
-    console.log("updateinitialObjectArray", initialObjectArray);
-    console.log("updateStuff", {
-      [name]: event.target.value,
-    });
-    console.log("updateStuff2", updateObjectArray[index]);
 
     setUpdateObjectArray([
       ...updateObjectArray.slice(0, index),
-      { ...intermediaryObject, [name]: event.target.value },
+      { ...updateObjectArray[index], [name]: event.target.value },
       ...updateObjectArray.slice(index + 1, initialObjectArray.length),
     ]);
   };
-  console.log("localModalData", localModalData);
+
   const textEditors = Object.keys(localModalData).map(
-    (element: string, index: number) => {
+    (element: string) => {
       if (
         [
           "video",
@@ -139,36 +125,44 @@ export const EditModal = (props: EditModalProps) => {
             </StyledText>
             {Array.isArray(localModalData[element]) ? (
               localModalData[element].map((item: any, index: number) => {
-                // typeof item === "object" &&
-                // console.log("item", Object.keys(item));
                 if (typeof item === "object") {
-                  return Object.keys(item).map((objData: string) => {
-                    return (
-                      <>
-                        {Array.isArray(item[objData]) ? (
-                          <></>
-                        ) : (
+                  return (
+                    <>
+                      <StyledText color={colors.primary.base}>
+                        {"Element "}
+                        {index + 1}
+                        {" from "}
+                        {capitalizeFirstLetter(element)}
+                      </StyledText>
+                      {Object.keys(item).map((objData: string) => {
+                        return (
                           <>
-                            <StyledText color={colors.primary.base}>
-                              {capitalizeFirstLetter(objData)}{" "}
-                            </StyledText>
-                            <StyledTextArea
-                              onChange={(e) =>
-                                handleObjectArrayMessageChange(
-                                  e,
-                                  objData,
-                                  index
-                                )
-                              }
-                              name={item}
-                            >
-                              {item[objData]}
-                            </StyledTextArea>
+                            {Array.isArray(item[objData]) ? (
+                              <></>
+                            ) : (
+                              <>
+                                <StyledText color={colors.primary.base}>
+                                  {capitalizeFirstLetter(objData)}{" "}
+                                </StyledText>
+                                <StyledTextArea
+                                  onChange={(e) =>
+                                    handleObjectArrayMessageChange(
+                                      e,
+                                      objData,
+                                      index
+                                    )
+                                  }
+                                  name={item}
+                                >
+                                  {item[objData]}
+                                </StyledTextArea>
+                              </>
+                            )}
                           </>
-                        )}
-                      </>
-                    );
-                  });
+                        );
+                      })}
+                    </>
+                  );
                 }
                 return (
                   <>
