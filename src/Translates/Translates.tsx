f68300501bf00ea2217a00582036c834ef5eefe4
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-10-25 16:34:02 +0300

    loading

--- src/Translates/Translates.tsx
+++ src/Translates/Translates.tsx
@@ -20,7 +20,7 @@ export const StoryBoxFromTranslates = {
     en: ["First name of the person receiving the gift *", "Which characters / elements of nature do you want the story to be about? *", "What is the relationship between you and the person you want to make the story for? *", "Is there any special situation at which do you want to refer to in the story? (optional)", "The event for which you want to give the gift: *", "The date you want to receive the gift: *", "Describe in 2-3 sentences, the context / real situation between you. *", "Other details: (the first name of the person who Gives the story, the message you want to convey) *", "I want the story in the format: *", "Select a format","Audio on the YouTube channel ' Profa de Povesti'", "Recorded on DVD audio", "Print","Delivery address *"],
     fr: ["Prénom de la personne qui reçoit le cadeau*", "De quels personnages/éléments de la nature voulez-vous que l'histoire parle ?*", "Quelle est la relation entre vous et la personne pour qui vous voulez faire l'histoire ? *", "Y a-t-il une situation particulière à laquelle vous souhaitez faire référence dans l'histoire ? (facultatif)", "L'événement pour lequel vous souhaitez offrir le cadeau : *", "La date à laquelle vous souhaitez recevoir le cadeau :*", "Décrivez en 2-3 phrases, le contexte/situation réelle entre vous.*", "Autres détails : (le prénom de la personne qui raconte l'histoire, le message que vous souhaitez faire passer)*", " Je veux l'histoire au format : *", "Sélectionner un format","Audio sur la chaîne YouTube 'Profa de Povesti'", "Enregistré sur DVD audio", "Imprimer","Adresse de livraison *"],
 }
-export const courseButtonTranslates={
+export const courseButtonTranslates = {
     ro: "Cumpără",
     en: "Buy",
     fr: "Acheter",
