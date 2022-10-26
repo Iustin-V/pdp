b2dcc7818b5c04625f21c4293d3eaa406166d018
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-10-26 08:44:22 +0300

    changed in loading component

--- src/comp/Loading/Loading.js
+++ src/comp/Loading/Loading.js
@@ -1,9 +1,7 @@
 import {CloudStyled, ImageStyled, LoadingContainer, LogoImageContainer, TeacherIconStyled} from "./LoadingStyle";
-import Typewriter from 'typewriter-effect';
 import {loadingTranslates} from "../../Translates/Translates";
 
 const Loading = () => {
-    // @ts-ignore
     return (
         <>
             <LoadingContainer>
@@ -13,16 +11,7 @@ const Loading = () => {
                 </div>
                 <TeacherIconStyled/>
                 <CloudStyled>
-                    <Typewriter
-                        class="typewriter"
-                        onInit={(typewriter) => {
-                            typewriter.pauseFor(500)
-                        }}
-                        options={{
-                            strings: [`<span style="font-size: 30px">${loadingTranslates[localStorage?.locale || "ro"]}</span>`],
-                            autoStart: true,
-                            loop: true,
-                        }}/>
+                    {loadingTranslates[localStorage?.locale || "ro"]}
                 </CloudStyled>
             </LoadingContainer>
         </>
